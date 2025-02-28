3efca9b18d38aa514933d1675adbb3d868bc67a5
Theristes <theristes@gmail.com>
2025-02-28 00:44:44 -0300

    working on pt-br

---
+++ install/language/pt-br/install/step_1.php
@@ -0,0 +1,82 @@
+<?php
+// Heading
+$_['heading_title'] = 'Acordo de Licença';
+
+// Text
+$_['text_step_1']   = 'Por favor, leia o acordo de licença do OpenCart';
+$_['text_terms']    = '<h3>LICENÇA PÚBLICA GERAL GNU</h3>
+<p>Versão 3, 29 de junho de 2007</p>
+<p>Copyright &copy; 2007 Free Software Foundation, Inc. &lt;http://fsf.org/&gt;</p>
+<p> Todos têm permissão para copiar e distribuir cópias verbatim
+  deste documento de licença, mas alterá-lo não é permitido.</p>
+<h3><a name="preamble"></a>Preâmbulo</h3>
+<p>A Licença Pública Geral GNU é uma licença livre, copyleft para
+  software e outros tipos de obras.</p>
+<p>As licenças para a maioria dos softwares e outras obras práticas são projetadas
+  para tirar sua liberdade de compartilhar e alterar as obras. Em contraste,
+  a Licença Pública Geral GNU é destinada a garantir sua liberdade de
+  compartilhar e alterar todas as versões de um programa - para garantir que ele permaneça software livre para todos os seus usuários. Nós, a Free Software Foundation, usamos a
+  Licença Pública Geral GNU para a maioria de nossos softwares; ela se aplica também a
+  qualquer outra obra lançada desta forma por seus autores. Você pode aplicá-la aos
+  seus programas também.</p>
+<p>Quando falamos de software livre, estamos nos referindo à liberdade, não
+  ao preço. Nossas Licenças Públicas Gerais são projetadas para garantir que você
+  tenha a liberdade de distribuir cópias de software livre (e cobrar por
+  elas se desejar), que você receba o código-fonte ou possa obtê-lo se
+  quiser, que você possa alterar o software ou usar partes dele em novos
+  programas livres, e que você saiba que pode fazer essas coisas.</p>
+<p>Para proteger seus direitos, precisamos impedir que outros neguem a você
+  esses direitos ou peçam que você os abra mão. Portanto, você tem
+  certas responsabilidades se distribuir cópias do software, ou se
+  modificá-lo: responsabilidades de respeitar a liberdade dos outros.</p>
+<p>Por exemplo, se você distribuir cópias de tal programa, seja
+  gratuitamente ou por uma taxa, você deve passar aos destinatários as mesmas
+  liberdades que você recebeu. Você deve garantir que eles também recebam
+  ou possam obter o código-fonte. E você deve mostrar a eles estes termos para que
+  saibam seus direitos.</p>
+<p>Os desenvolvedores que usam a GPL GNU protegem seus direitos com dois passos:
+  (1) afirmar o copyright sobre o software, e (2) oferecer a você esta Licença
+  dando-lhe permissão legal para copiar, distribuir e/ou modificar o software.</p>
+<p>Para a proteção dos desenvolvedores e autores, a GPL explica claramente
+  que não há garantia para este software livre. Para o bem dos usuários e
+  autores, a GPL exige que versões modificadas sejam marcadas como
+  alteradas, para que seus problemas não sejam atribuídos erroneamente aos
+  autores de versões anteriores.</p>
+<p>Alguns dispositivos são projetados para negar aos usuários acesso para instalar ou executar
+  versões modificadas do software dentro deles, embora o fabricante
+  possa fazer isso. Isso é fundamentalmente incompatível com o objetivo de
+  proteger a liberdade dos usuários de alterar o software. O padrão sistemático
+  de tal abuso ocorre na área de produtos para uso individual,
+  que é precisamente onde é mais inaceitável. Portanto, projetamos esta versão da GPL para proibir a prática para esses
+  produtos. Se tais problemas surgirem substancialmente em outros domínios, estamos prontos para estender esta disposição a esses domínios em versões futuras
+  da GPL, conforme necessário para proteger a liberdade dos usuários.</p>
+<p>Finalmente, todo programa é constantemente ameaçado por patentes de software.
+  Os estados não devem permitir que patentes restrinjam o desenvolvimento e uso de
+  software em computadores de uso geral, mas naqueles que o fazem, desejamos
+  evitar o perigo especial de que patentes aplicadas a um programa livre possam
+  torná-lo efetivamente proprietário. Para evitar isso, a GPL assegura que
+  patentes não possam ser usadas para tornar o programa não-livre.</p>
+<p>Os termos e condições precisos para cópia, distribuição e
+  modificação seguem.</p>
+<h3><a name="terms"></a>TERMOS E CONDIÇÕES</h3>
+<h4><a id="section0"></a>0. Definições.</h4>
+<p>&ldquo;Esta Licença&rdquo; refere-se à versão 3 da Licença Pública Geral GNU.</p>
+<p>&ldquo;Copyright&rdquo; também significa leis semelhantes ao copyright que se aplicam a outros tipos de
+  obras, como máscaras de semicondutores.</p>
+<p>&ldquo;O Programa&rdquo; refere-se a qualquer obra passível de copyright licenciada sob esta
+  Licença. Cada licenciado é chamado de &ldquo;você&rdquo;. &ldquo;Licenciados&rdquo; e
+  &ldquo;destinatários&rdquo; podem ser indivíduos ou organizações.</p>
+<p>Modificar uma obra significa copiar ou adaptar toda ou parte da obra
+  de uma maneira que exija permissão de copyright, exceto a criação de uma
+  cópia exata. A obra resultante é chamada de &ldquo;versão modificada&rdquo; da
+  obra anterior ou uma obra &ldquo;baseada&rdquo; na obra anterior.</p>
+<p>Uma &ldquo;obra coberta&rdquo; significa tanto o Programa não modificado quanto uma obra baseada
+  no Programa.</p>
+<p>Propagar uma obra significa fazer qualquer coisa com ela que, sem
+  permissão, tornaria você direta ou indiretamente responsável por
+  infração sob a lei de copyright aplicável, exceto executá-la em um
+  computador ou modificar uma cópia privada. Propagação inclui cópia,
+  distribuição (com ou sem modificação), disponibilização ao
+  público, e em alguns países outras atividades também.</p>
+<p>Conveying uma obra significa qualquer tipo de propagação que permita a outras
+  partes
---
+++ install/language/pt-br/pt-br.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_name']       = 'Português (Brasil)';
+$_['text_loading']    = 'Carregando...';
+
+// Buttons
+$_['button_continue'] = 'Continuar';
+$_['button_back']     = 'Voltar';
+
+// Error
+$_['error_exception'] = 'Código de Erro(%s): %s em %s na linha %s';
