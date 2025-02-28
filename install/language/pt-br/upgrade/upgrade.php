7738ba583ab722042a6e6e40153354ab28fa593f
Theristes <theristes@gmail.com>
2025-02-28 00:44:44 -0300

    working on pt-br

---
+++ install/language/pt-br/upgrade/upgrade.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']     = 'Atualização';
+
+// Text
+$_['text_upgrade']      = 'Prepare-se para sua atualização';
+$_['text_server']       = 'Verifique se o seu servidor está configurado corretamente';
+$_['text_steps']        = 'Progresso da Atualização';
+$_['text_error']        = 'Poste quaisquer problemas de erros de script de atualização nos fóruns';
+$_['text_clear']        = 'Após a atualização, limpe todos os cookies no seu navegador para evitar erros de token.';
+$_['text_admin']        = 'Por favor, insira o nome do diretório admin';
+$_['text_refresh']      = 'Carregue a página de administração e pressione Ctrl+F5 duas vezes para forçar o navegador a atualizar as alterações de CSS.';
+$_['text_user']         = 'Vá para Admin -> Usuários -> Grupos de Usuários e Edite o grupo de Administrador Principal. Marque todas as caixas.';
+$_['text_setting']      = 'Vá para Admin e Edite as Configurações do Sistema principal. Atualize todos os campos e clique em salvar, mesmo que nada tenha mudado.';
+$_['text_store']        = 'Carregue a loja e pressione Ctrl+F5 duas vezes para forçar o navegador a atualizar as alterações de CSS.';
+$_['text_success']      = 'Parabéns! Você atualizou com sucesso sua instalação do OpenCart!';
+$_['text_patch']        = 'Patch %s foi aplicado (%s de %s)';
+
+// Entry
+$_['entry_admin']       = 'Nome do Admin';
+$_['entry_progress']    = 'Progresso';
+
+// Error
+$_['error_admin']       = 'Erro: arquivo de configuração %s não encontrado. Certifique-se de que o diretório admin tem o nome correto!';
+$_['error_file']        = 'Erro: arquivo de configuração %s não encontrado!';
+$_['error_writable']    = 'Erro: arquivo de configuração %s não é gravável!';
+$_['error_upgrade']     = 'Erro: Nenhum arquivo de atualização definido!';
+$_['error_server']      = 'Erro: constante HTTP_SERVER não encontrada!';
+$_['error_catalog']     = 'Erro: constante HTTP_CATALOG não encontrada!';
+$_['error_db_driver']   = 'Erro: constante DB_DRIVER não encontrada!';
+$_['error_db_hostname'] = 'Erro: constante DB_HOSTNAME não encontrada!';
+$_['error_db_username'] = 'Erro: constante DB_USERNAME não encontrada!';
+$_['error_db_password'] = 'Erro: constante DB_PASSWORD não encontrada!';
+$_['error_db_database'] = 'Erro: constante DB_DATABASE não encontrada!';
+$_['error_db_port']     = 'Erro: constante DB_PORT não encontrada!';
+$_['error_db_prefix']   = 'Erro: constante DB_PREFIX não encontrada!';
