bd1517a77156af6a2b2d881cd7d7d699fbdba127
Theristes <theristes@gmail.com>
2025-02-28 14:01:04 -0300

    pay on site

---
+++ extension/opencart/catalog/language/pt-br/payment/pay_on_site.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']        = 'Pagar no Local';
+
+// Text
+$_['text_title']           = 'Pagar no Local';
+
+// Error
+$_['error_order_id']       = 'Nenhum ID de pedido na sessão!';
+$_['error_payment_method'] = 'Método de pagamento incorreto!';
