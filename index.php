2cd08899af17391d0efe5bf0608a26910f441810
Theristes <theristes@gmail.com>
2025-04-14 11:37:49 -0300

    getting back

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
-echo("HELLO WORD")
-// // Version
-// define('VERSION', '4.1.0.1');
+// Version
+define('VERSION', '4.1.0.1');
 
-// // Configuration
-// if (is_file('config.php')) {
-// 	require_once('config.php');
-// }
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
 
-// // Install
-// if (!defined('DIR_APPLICATION')) {
-// 	header('Location: install/index.php');
-// 	exit();
-// }
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit();
+}
 
-// // Startup
-// require_once(DIR_SYSTEM . 'startup.php');
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
 
-// // Framework
-// require_once(DIR_SYSTEM . 'framework.php');
+// Framework
+require_once(DIR_SYSTEM . 'framework.php');
