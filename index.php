207836c103e5cc2857881fdb6d14afb78edc59dd
Theristes <theristes@gmail.com>
2025-04-16 11:51:59 -0300

    moving debug

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ define('VERSION', '4.1.0.1');
 
 require_once('config.php');
 
+require_once(DIR_SYSTEM . 'startup.php');
+
 
 echo 'DB_DATABASE: ' . DB_DATABASE . '<br>';
 echo 'DB_HOSTNAME: ' . DB_HOSTNAME . '<br>';
 exit;
-
-
-require_once(DIR_SYSTEM . 'startup.php');
 
 require_once(DIR_SYSTEM . 'framework.php');
