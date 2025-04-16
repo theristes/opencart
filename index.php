c37677a8306b74f9639661bdff3f3eac905678b1
Theristes <theristes@gmail.com>
2025-04-16 20:28:44 -0300

    commit index.php

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ if (is_file('config.php')) {
 	require_once('config.php');
 }
 
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit();
+}
+
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
 
