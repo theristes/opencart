b1f802f2e66e6b740b720f77b50366b44b6cbe42
Theristes <theristes@gmail.com>
2025-04-16 11:49:53 -0300

    debug index

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ define('VERSION', '4.1.0.1');
 
 require_once('config.php');
 
+
+echo 'DB_DATABASE: ' . DB_DATABASE . '<br>';
+echo 'DB_HOSTNAME: ' . DB_HOSTNAME . '<br>';
+exit;
+
+
 require_once(DIR_SYSTEM . 'startup.php');
 
 require_once(DIR_SYSTEM . 'framework.php');
