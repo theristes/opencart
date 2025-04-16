58de81bf032baf11a6317ad7acb196c4c4759b40
Theristes <theristes@gmail.com>
2025-04-16 11:57:48 -0300

    trying to change db_autostart

--- system/config/catalog.php
+++ system/config/catalog.php
@@ -3,7 +3,7 @@
 $_['site_url']           = HTTP_SERVER;
 
 // Database
-$_['db_autostart']       = true;
+$_['db_autostart']       = false;
 $_['db_engine']          = DB_DRIVER; // mysqli, pdo or pgsql
 $_['db_hostname']        = DB_HOSTNAME;
 $_['db_username']        = DB_USERNAME;
