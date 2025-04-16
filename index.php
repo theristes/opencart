a77232911cd43bbdfa47387922f17fd37b1b207d
Theristes <theristes@gmail.com>
2025-04-16 20:27:42 -0300

    moving back is_file config.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
+// Version
 define('VERSION', '4.1.0.1');
 
-require_once('config.php');
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
 
+// Startup
 require_once(DIR_SYSTEM . 'startup.php');
 
+// Framework
 require_once(DIR_SYSTEM . 'framework.php');
-
-exit;
