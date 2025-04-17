02da0877124eb1ab981adf52b4fef01604bc702f
Theristes <theristes@gmail.com>
2025-04-16 21:19:07 -0300

    changing header images

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -53,13 +53,13 @@ class Header extends \Opencart\System\Engine\Controller {
 
 		// Fav icon
 		if (is_file(DIR_IMAGE . $this->config->get('config_icon'))) {
-			$data['icon'] = $this->config->get('config_url') . 'image/' . $this->config->get('config_icon');
+			$data['icon'] = $this->config->get('config_url') . 'images/' . $this->config->get('config_icon');
 		} else {
 			$data['icon'] = '';
 		}
 
 		if (is_file(DIR_IMAGE . $this->config->get('config_logo'))) {
-			$data['logo'] = $this->config->get('config_url') . 'image/' . $this->config->get('config_logo');
+			$data['logo'] = $this->config->get('config_url') . 'images/' . $this->config->get('config_logo');
 		} else {
 			$data['logo'] = '';
 		}
