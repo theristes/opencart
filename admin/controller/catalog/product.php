e4f8013d3b6ca7d3f72bcadd3cf57557d4654aff
Theristes <theristes@gmail.com>
2025-04-18 15:56:07 -0300

    chagin image

--- admin/controller/catalog/product.php
+++ admin/controller/catalog/product.php
@@ -346,7 +346,8 @@ class Product extends \Opencart\System\Engine\Controller {
 
 		foreach ($results as $result) {
 			if ($result['image'] && is_bucket_file(html_entity_decode($result['image'], ENT_QUOTES, 'UTF-8'))) {
-				$image = $result['image'];
+				$image =  html_entity_decode($result['image'], ENT_QUOTES, 'UTF-8');
+				//$result['image'];
 			} else {
 				$image = 'no_image.png';
 			}
