83cc0ea8da4aaf5e106f6cbfe25b1ed17c78d417
Theristes <theristes@gmail.com>
2025-04-22 15:56:43 -0300

    adding on_spot

--- extension/opencart/catalog/controller/payment/on_spot.php
+++ extension/opencart/catalog/controller/payment/on_spot.php
@@ -1,19 +1,22 @@
 <?php
-namespace Opencart\Catalog\Controller\Payment;
+namespace Opencart\Catalog\Controller\Extension\Opencart\Payment;
 
 class OnSpot extends \Opencart\System\Engine\Controller {
-    public function index(): string {
-        $this->load->language('payment/on_spot');
-        return $this->load->view('payment/on_spot');
+    public function index(array $settings = []): string {
+        $this->load->language('extension/opencart/payment/on_spot');
+        return $this->load->view('extension/opencart/payment/on_spot');
     }
 
-    public function confirm(): void {
+    public function confirm(array $route = []): void {
         if ($this->session->data['payment_method']['code'] === 'on_spot') {
+            $this->load->language('extension/opencart/payment/on_spot');
             $this->load->model('checkout/order');
+
             $this->model_checkout_order->addHistory(
                 $this->session->data['order_id'],
-                1,
-                'Customer will pay on spot.'
+                $this->config->get('payment_on_spot_order_status_id'),
+                $this->language->get('text_description'),
+                true
             );
         }
     }
