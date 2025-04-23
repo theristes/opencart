eda2f52622e8d1146589c1a010e010b88a0aa0d5
Theristes <theristes@gmail.com>
2025-04-23 10:11:19 -0300

    removing old payment methods

--- extension/opencart/catalog/language/pt-br/payment/bank_transfer.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title']    = 'Transferência Bancária';
-
-// Text
-$_['text_instruction'] = 'Instruções para Transferência Bancária';
-$_['text_description'] = 'Por favor, transfira o valor total para a seguinte conta bancária.';
-$_['text_payment']     = 'Seu pedido não será enviado até que recebamos o pagamento.';
