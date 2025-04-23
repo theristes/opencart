66bc42421be90b1b379c11ba00daa6741eb945c9
Theristes <theristes@gmail.com>
2025-04-23 19:29:07 -0300

    payment method how it is

--- catalog/model/checkout/payment_method.php
+++ catalog/model/checkout/payment_method.php
@@ -25,7 +25,7 @@ class PaymentMethod extends \Opencart\System\Engine\Model {
 
 		foreach ($results as $result) {
 			if ($this->config->get('payment_' . $result['code'] . '_status')) {
-				$this->load->model('extension/' . $result['extension'] . '/'. 'catalog'. '/'. 'controller'.'/'.'payment'.'/' . $result['code']);
+				$this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
 
 				$key = 'model_extension_' . $result['extension'] . '_payment_' . $result['code'];
 
