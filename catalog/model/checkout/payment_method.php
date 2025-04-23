159488968fb98e01beee60ddfb5d37d65034cb88
Theristes <theristes@gmail.com>
2025-04-23 19:08:17 -0300

    debug payment methods

--- catalog/model/checkout/payment_method.php
+++ catalog/model/checkout/payment_method.php
@@ -22,30 +22,31 @@ class PaymentMethod extends \Opencart\System\Engine\Model {
 		$this->load->model('setting/extension');
 
 		$results = $this->model_setting_extension->getExtensionsByType('payment');
+		echo($results);
 
-		foreach ($results as $result) {
-			if ($this->config->get('payment_' . $result['code'] . '_status')) {
-				$this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
+		// foreach ($results as $result) {
+		// 	if ($this->config->get('payment_' . $result['code'] . '_status')) {
+		// 		$this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
 
-				$key = 'model_extension_' . $result['extension'] . '_payment_' . $result['code'];
+		// 		$key = 'model_extension_' . $result['extension'] . '_payment_' . $result['code'];
 
-				if ($this->{$key}->getMethods) {
-					$payment_methods = $this->{$key}->getMethods($payment_address);
+		// 		if ($this->{$key}->getMethods) {
+		// 			$payment_methods = $this->{$key}->getMethods($payment_address);
 
-					if ($payment_methods) {
-						$method_data[$result['code']] = $payment_methods;
-					}
-				}
-			}
-		}
+		// 			if ($payment_methods) {
+		// 				$method_data[$result['code']] = $payment_methods;
+		// 			}
+		// 		}
+		// 	}
+		// }
 
-		$sort_order = [];
+		// $sort_order = [];
 
-		foreach ($method_data as $key => $value) {
-			$sort_order[$key] = $value['sort_order'];
-		}
+		// foreach ($method_data as $key => $value) {
+		// 	$sort_order[$key] = $value['sort_order'];
+		// }
 
-		array_multisort($sort_order, SORT_ASC, $method_data);
+		// array_multisort($sort_order, SORT_ASC, $method_data);
 
 		return $method_data;
 	}
