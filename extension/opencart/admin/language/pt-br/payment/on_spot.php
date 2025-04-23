668d822c251ab58086dd80ca1584b2a81c282dc2
Theristes <theristes@gmail.com>
2025-04-23 06:43:53 -0300

    removing the others payments

--- extension/opencart/admin/language/pt-br/payment/on_spot.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_['heading_title'] = 'No Local';
-$_['text_success'] = 'Successo: Você atualizou seu pagamento!';
-$_['error_permission'] = 'Atenção: Você não tem permissão para fazer essa alteração!';
