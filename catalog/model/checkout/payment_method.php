4ea6437a6fa1c9e89cd6b59453f38daff6bcd46e
Theristes <theristes@gmail.com>
2025-04-23 19:13:00 -0300

    payment extension status

--- catalog/model/checkout/payment_method.php
+++ catalog/model/checkout/payment_method.php
@@ -26,7 +26,7 @@ class PaymentMethod extends \Opencart\System\Engine\Model {
 
 		foreach ($results as $result) {
 			if ($this->config->get('payment_' . $result['code'] . '_status')) {
-				$this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
+				// $this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
 
 				// $key = 'model_extension_' . $result['extension'] . '_payment_' . $result['code'];
 
