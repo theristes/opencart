d57ebbbf88b25e82a10f42deda0a85c5f769eb02
Theristes <theristes@gmail.com>
2025-05-19 23:52:13 -0300

    adding payment correctly

--- catalog/model/localisation/service.php
+++ catalog/model/localisation/service.php
@@ -1,17 +1,17 @@
 <?php
 namespace Opencart\Catalog\Model\Localisation;
 
-class Payment extends \Opencart\System\Engine\Model {
-	public function getAllowedPaymentIdsByService(int $service_id): array {
-		$query = $this->db->query("
-			SELECT p.description 
+class Service extends \Opencart\System\Engine\Model {
+	public function getServices(): array {
+		$sql = "
+			SELECT se.id, se.code, CONCAT(se.addressStreet, ', ', se.addressNumber, ' - ', se.addressNeighborhood, ', ', se.addressCity) AS name
 			FROM farmax_shop.store s
-			INNER JOIN farmax_shop.service se ON s.id = se.storeId
-			INNER JOIN farmax_shop.payment p ON se.id = p.serviceId
-			WHERE s.storeName = DATABASE() AND se.id = '" . (int)$service_id . "'
-		");
+			INNER JOIN farmax_shop.service se ON se.storeId = s.id
+			WHERE s.storeName = DATABASE()
+		";
 
-		// `description` is assumed to match the payment method `code` (e.g., 'cod', 'bank_transfer')
-		return array_column($query->rows, 'description');
+		$query = $this->db->query($sql);
+		return $query->rows;
 	}
 }
+
