1f092c686823a7a2277645decf3015a20f633202
Theristes <theristes@gmail.com>
2025-07-02 10:01:35 -0300

    replacing métodos by formas

--- catalog/language/pt-br/account/payment_method.php
+++ catalog/language/pt-br/account/payment_method.php
@@ -4,4 +4,4 @@ $_['heading_title']   = 'Formas de Pagamento';
 
 // Text
 $_['text_account']    = 'Conta';
-$_['text_no_results'] = 'Você não tem métodos de pagamento em sua conta.';
+$_['text_no_results'] = 'Você não tem formas de pagamento em sua conta.';
