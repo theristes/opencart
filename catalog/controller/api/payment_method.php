93c44364f2f3c73db8f026557ade9dcc3451b7bd
Theristes <theristes@gmail.com>
2025-07-15 16:24:57 -0300

    payment method

--- catalog/controller/api/payment_method.php
+++ catalog/controller/api/payment_method.php
@@ -39,7 +39,8 @@ class PaymentMethod extends \Opencart\System\Engine\Controller {
 
 		// 4. Validate payment address if required
 		if ($this->config->get('config_checkout_payment_address') && !isset($this->session->data['payment_address'])) {
-			$output['error'] = $this->language->get('error_payment_address');
+			// $output['error'] = $this->language->get('error_payment_address');
+			$output['error'] = "Hola que tal";
 		}
 
 		// 5. Validate payment Method
