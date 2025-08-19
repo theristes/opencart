1fc43e64499e0bcba1e9a09d20f394e472a20e10
Theristes <theristes@gmail.com>
2025-08-19 16:00:41 -0300

    confirm method

--- catalog/controller/checkout/confirm.php
+++ catalog/controller/checkout/confirm.php
@@ -336,7 +336,6 @@ class Confirm extends \Opencart\System\Engine\Controller {
 
 		if ($status && $extension_info) {
 			$data['payment'] = $this->load->controller('extension/' . $extension_info['extension'] . '/payment/' . $extension_info['code']);
-			echo $data['payment'];
 		} else {
 			$data['payment'] = '';
 		}
